2589e17902fe60478ce59c6488701ce358f056d5
VadymDrebot <72618090+VadymDrebot@users.noreply.github.com>
2020-12-14 20:06:25 +0200

    its a comment

---
+++ file.py
@@ -0,0 +1,2 @@
+with open("../raznoe/hello", "a") as somefile:
+    somefile.write("  \nhello !!!!!")
---
+++ конкатенация строк.py
@@ -0,0 +1,7 @@
+a=input("Input first string: ")
+b=input("Input second string: ")
+c = a + " " + b
+print("Concatenation: ",c)
+import aaaaa
+if input("calc? ")=="y":
+    aaaaa.ffff()
